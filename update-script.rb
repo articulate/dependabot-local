5f2a5f374f3d42d544c6f1352d07fd9c37562129
Kevin Stewart <kdstew@gmail.com>
2019-08-22 08:57:29 -0400

    renames and moving stuff around

--- update-script.rb
+++ update-script.rb
@@ -9,12 +9,14 @@ require "yaml"
 require 'rubygems'
 require 'git'
 
+REPO_DIRECTORY  = "/service/repo" # Directory where the repository being updated is volumed in
 
+####### Monkey patching FileFetchers to read source files from disk
+FileStruct = Struct.new(:name, :path, :type, :size)
 
-####### Monkey patching to get reading source files from disk
 module Dependabot
-  module NpmAndYarn
-    class FileFetcher
+  module FileFetchers
+    class Base
       def _fetch_file_content(path, fetch_submodules: false)
         if Pathname.new(path).exist?
           File.open(path, 'r') do |f|
@@ -24,45 +26,41 @@ module Dependabot
           raise Dependabot::DependencyFileNotFound, path
         end
       end
+
       def fetch_file_if_present(path, type: "file", fetch_submodules: false)
-        fullPath = '/service/repo/' + path
+        fullPath = File.join(REPO_DIRECTORY, path)
         fetch_file_from_host(path, fetch_submodules: fetch_submodules) if Pathname.new(fullPath).exist?
+      end
+
+      def _fetch_repo_contents(path, raise_errors: true, fetch_submodules: false)
+        base_dir = File.join(REPO_DIRECTORY, path)
+        Dir['*', base: base_dir].map do |file|
+          type = File.file?(File.join(base_dir, file)) ? 'file' : 'dir'
+          FileStruct.new(file, base_dir, type, 0)
+        end
       end
     end
   end
 end
 ####### END Monkey patch
 
-# Directory where the base dependency files are.
-REPO_DIRECTORY = "/service/repo"
-PACKAGE_MANAGER = "npm_and_yarn"
+Source           = Struct.new(:provider, :directory)
+DependencyUpdate = Struct.new(:name, :ignore_versions, :commit_message)
 
-Dependency = Struct.new(:name, :ignore_versions, :commit_message)
-
+git    = Git.open(REPO_DIRECTORY)
 config = YAML.load(File.read("config.yml"))
 
-dependencies = config.map do |(name, ignore_versions, commit_message)|
-  Dependency.new(name, ignore_versions, commit_message)
-end
+PACKAGE_MANAGER = config["package_manager"]
 
-git = Git.open('/service/repo')
-
-class Source
-  attr_accessor :provider, :repo, :directory, :branch
-
-  def initialize(provider, directory)
-    @provider = provider
-    @repo = nil
-    @directory = directory
-    @branch = nil
-  end
+dependency_updates = config["dependency_updates"].map do |dependency_update|
+  DependencyUpdate.new(dependency_update["name"], dependency_update["ignore_versions"], dependency_update["commit_message"])
 end
 
 def updateDependency(dependency_name, ignore_versions)
   puts "Updating #{dependency_name}.  Ignoring versions #{ignore_versions}"
 
   credentials = []
-  source = Source.new('local', REPO_DIRECTORY)
+  source = Source.new('file_system', REPO_DIRECTORY)
 
   #############################
   # Fetch the dependency files #
@@ -94,8 +92,6 @@ def updateDependency(dependency_name, ignore_versions)
     ignored_versions: [ignore_versions]
   )
 
-  checker.up_to_date?
-  checker.can_update?(requirements_to_unlock: :own)
   updated_deps = checker.updated_dependencies(requirements_to_unlock: :own)
 
   #####################################
@@ -126,7 +122,7 @@ def updateDependency(dependency_name, ignore_versions)
   return true
 end
 
-dependencies.each do |dependency|
+dependency_updates.each do |dependency|
   updatedFiles = updateDependency(dependency.name, dependency.ignore_versions)
 
   if updatedFiles
